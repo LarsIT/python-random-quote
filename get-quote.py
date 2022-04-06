3bd70e5fc2a8178a8b4abf1151965565661c7a73
Lars Baumann <102919582+LarsIT@users.noreply.github.com>
2022-04-06 13:56:40 +0200

    modified primary function

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,14 @@
+from random import Random
+
+
 def primary():
-  print("Keep it logically awesome.")
+  #print("Keep it logically awesome.")
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
-  print(quotes)
+  print(quotes[0])
 
 if __name__== "__main__":
   primary()
